32b6ce2065c044dd01f41564917b9bf1d81fcfc2
Kazuma Nagano <nagano.kazuma.na5@gmail.com>
2019-10-17 10:24:22 +0900

    update formula

--- brew-file.rb
+++ brew-file.rb
@@ -1,10 +1,10 @@
 class BrewFile < Formula
   desc "Brewfile manager for Homebrew."
-  homepage "https://github.com/rcmdnk/homebrew-file/"
-  url "https://github.com/rcmdnk/homebrew-file/archive/v7.0.2.tar.gz"
+  homepage "https://github.com/kazumanagano/homebrew-file/"
+  url "https://github.com/kazumanagano/homebrew-file/archive/v7.0.3.tar.gz"
   sha256 "3dd25b699d4208907da1d6969f531b0a21efe363c2b9d252efc531a6426da832"
 
-  head "https://github.com/rcmdnk/homebrew-file.git"
+  head "https://github.com/kazumanagano/homebrew-file.git"
 
   option "without-completions", "Disable bash/zsh completions"
 
